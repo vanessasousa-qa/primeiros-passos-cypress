52e1e0bceeb7d95971b25fe97f4d02bf5db07314
Vanessa <260506996+vanessasousa-qa@users.noreply.github.com>
2026-04-10 17:10:53 +0100

    User update scenario added

---
+++ cypress/e2e/user.spec.cy.js
@@ -0,0 +1,56 @@
+import userData from '../fixtures/userData.json'
+
+describe('Orange HRM Tests', () => {
+
+  const selectorsList = {
+    usernameField: "[name='username']",
+    passwordFild: "[name='password']",
+    loginButton: "[type='submit']",
+    sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
+    dashboardGrid: ".orangehrm-dashboard-grid",
+    wrongCredentialAlert: "[role='alert']",
+    myInfoButton: "[href='/web/index.php/pim/viewMyDetails']",
+    firtNameField: "[name='firstName']",
+    middleNameField: "[name='middleName']",
+    lastNameField: "[name='lastName']",
+    genericField: ".oxd-input--active",
+    dateField: "[placeholder='yyyy-dd-mm']",
+    dateCloseButton: ".--close",
+    genderButton: ".oxd-radio-wrapper",
+    submitButton: "[type='submit']"
+
+  }
+
+  it.only('User Info Update - Success', () => {
+    cy.visit('/auth/login')
+    cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
+    cy.get(selectorsList.passwordFild).type(userData.userSuccess.password)
+    cy.get(selectorsList.loginButton).click()
+    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+    cy.get(selectorsList.dashboardGrid),
+    cy.get(selectorsList.myInfoButton).click(),
+    cy.get(selectorsList.firtNameField).clear().type('Vanessa'),
+    cy.get(selectorsList.middleNameField).clear().type('Lynn'),
+    cy.get(selectorsList.lastNameField).clear().type('Montgomery'),
+    cy.get(selectorsList.genericField).eq(3).clear().type('Employee'),
+    cy.get(selectorsList.genericField).eq(4).clear().type('OtherID'),
+    cy.get(selectorsList.genericField).eq(5).clear().type('165348'),
+    cy.get(selectorsList.dateField).eq(0).clear().type('2029-05-03'),
+    cy.get(selectorsList.dateCloseButton).click(),
+    cy.get(selectorsList.dateField).eq(1).clear().type('1990-22-01'),
+    cy.get(selectorsList.dateCloseButton).click(),
+    cy.get(selectorsList.genderButton).eq(1).click(),
+    cy.get(selectorsList.genericField).eq(9).clear().type('Test_Field'),
+    cy.get(selectorsList.submitButton).eq(0).click(),
+    cy.get('body').should('contain', 'Successfully Updated'),
+    cy.get('.oxd-toast-close')
+
+  })
+  it('Login - Fail', () => {
+    cy.visit('/auth/login')
+    cy.get(selectorsList.usernameField).type(userData.userFail.username)
+    cy.get(selectorsList.passwordFild).type(userData.userFail.password)
+    cy.get(selectorsList.loginButton).click()
+    cy.get(selectorsList.wrongCredentialAlert)
+  })
+})
